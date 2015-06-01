00f0e0a02aa4e03bbe546abb81f32513606e47fc
mtnk121 <15001023878@sina.cn>
2015-06-01 13:26:37 +0800

    d

---
+++ app/src/main/java/com/secretlisa/demo/Fragment2.java
@@ -0,0 +1,20 @@
+package com.secretlisa.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hehaitao on 2015/6/1.
+ */
+public class Fragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2, container, false);
+    }
+}
--- app/src/main/java/com/secretlisa/demo/MainActivity.java
+++ app/src/main/java/com/secretlisa/demo/MainActivity.java
@@ -1,15 +1,12 @@
 package com.secretlisa.demo;
 
-import android.app.ActionBar;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
@@ -18,26 +15,20 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class  MainActivity  extends    FragmentActivity implements View.OnClickListener {
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
     private TextView tv_toutiao, tv_baike, tv_zixun, tv_jingying;
     private ImageView iv_choose1, iv_choose2, iv_choose3, iv_choose4;
-
     private ViewPager viewPager;
     private List<Fragment> contentFragments;
-    private FragmentManager manager;
     private List<Boolean> imageViewIsChoseList;
     private List<ImageView> imageViewList;
-
-//----------------------------
-
-
-    @Override
+//----------------------------------------------
+//    @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
         // ----------------------------------------------------------------------------------------
         tv_toutiao = (TextView) findViewById(R.id.tv_toutiao);
         tv_baike = (TextView) findViewById(R.id.tv_baike);
@@ -56,42 +47,35 @@ public class  MainActivity  extends    FragmentActivity implements View.OnClickL
         imageViewIsChoseList.add(false);
         imageViewIsChoseList.add(false);
         imageViewIsChoseList.add(false);
-        imageViewIsChoseList.add(false);
+
         imageViewList.add(iv_choose1);
         imageViewList.add(iv_choose2);
         imageViewList.add(iv_choose3);
         imageViewList.add(iv_choose4);
 
-
-
-
         tv_toutiao.setOnClickListener(this);
         tv_baike.setOnClickListener(this);
         tv_zixun.setOnClickListener(this);
         tv_jingying.setOnClickListener(this);
-
-
         addFragment();
-
     }
-
+//-----------------------------------------------------------------------------
     // 向ViewPager里面添加Fragment
     public void addFragment() {
         viewPager = (ViewPager) findViewById(R.id.index_pager);
         contentFragments = new ArrayList<Fragment>();
-        manager = getSupportFragmentManager();
-        HeadLineFragment fragment1 = new HeadLineFragment();
-        CyclopediaFragment fragment2 = new CyclopediaFragment();
-        InformationFragment fragment3 = new InformationFragment();
-        ManageFragment fragment4 = new ManageFragment();
+
+        Fragment1 fragment1 = new Fragment1();
+        Fragment2 fragment2 = new Fragment2();
+        Fragment3 fragment3 = new Fragment3();
+        Fragment4 fragment4 = new Fragment4();
 
         contentFragments.add(fragment1);
         contentFragments.add(fragment2);
         contentFragments.add(fragment3);
         contentFragments.add(fragment4);
-        viewPager.setAdapter(new MyAdapter(getSupportFragmentManager(),contentFragments));
+        viewPager.setAdapter(new MyAdapter(getSupportFragmentManager(), contentFragments));
         viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
             @Override
             public void onPageSelected(int arg0) {
                 for (int i = 0; i < imageViewIsChoseList.size(); i++) {
@@ -104,48 +88,37 @@ public class  MainActivity  extends    FragmentActivity implements View.OnClickL
                 imageViewList.get(arg0).setBackgroundColor(
                         Color.argb(255, 0, 170, 0));
             }
-
             @Override
             public void onPageScrolled(int arg0, float arg1, int arg2) {
                 // TODO Auto-generated method stub
-
             }
-
             @Override
             public void onPageScrollStateChanged(int arg0) {
                 // TODO Auto-generated method stub
-
             }
         });
-
     }
-
+//---------------------------------------------------------------------------------------
     private class MyAdapter extends FragmentStatePagerAdapter {
-
-        private  List<Fragment>  contentFragments;
+        private List<Fragment> contentFragments;
         public MyAdapter(FragmentManager fm, List<Fragment> contentFragments) {
             super(fm);
-            this.contentFragments=contentFragments;
+            this.contentFragments = contentFragments;
             // TODO Auto-generated constructor stub
         }
-
         public int getCount() {
             // TODO Auto-generated method stub
             return contentFragments.size();
-
         }
-
         @Override
         public Fragment getItem(int arg0) {
             // TODO Auto-generated method stub
             return contentFragments.get(arg0);
         }
-
     }
-
+//---------------------------------------------------------------------------------------------
     @Override
     public void onClick(View v) {
-
         switch (v.getId()) {
             case R.id.tv_toutiao:
                 int itemPostion0 = 0;
@@ -165,11 +138,9 @@ public class  MainActivity  extends    FragmentActivity implements View.OnClickL
                 break;
 
 
-
-
         }
     }
-
+//----------------------------------------------------------------------------------
     // 设置当前viewpager
     private void setCurrentView(int itemPostion) {
         if (viewPager.getCurrentItem() != itemPostion) {
@@ -185,9 +156,5 @@ public class  MainActivity  extends    FragmentActivity implements View.OnClickL
                     Color.argb(255, 0, 170, 0));
         }
     }
-
-
-
-
-
+//---------------------------------------------------------------------------------------
 }
