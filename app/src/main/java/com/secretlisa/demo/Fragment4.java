54cdf0499732a7f07eb45646c5084ce5f714c425
mtnk121 <15001023878@sina.cn>
2015-06-01 19:21:31 +0800

    commit

--- app/src/main/java/com/secretlisa/demo/Fragment4.java
+++ app/src/main/java/com/secretlisa/demo/Fragment4.java
@@ -3,6 +3,7 @@ package com.secretlisa.demo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,7 @@ import android.view.ViewGroup;
  */
 public class Fragment4 extends Fragment {
     @Nullable
-    @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment4, container, false);
     }
